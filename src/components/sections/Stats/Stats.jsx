6e4a4e70ac705e7cfd09ac7e8d74008a66614cc0
Vitaliy Tkachuk <vitfl@protonmail.com>
2019-08-14 23:00:01 +0300

    :fire: removing comments

--- src/components/sections/Stats/Stats.jsx
+++ src/components/sections/Stats/Stats.jsx
@@ -1,20 +1,15 @@
-// TODO:
-// https://github.com/chartjs/Chart.js
-
 import React from 'react';
 import { Container } from 'reactstrap';
 import SectionHeader from '@atoms/SectionHeader/SectionHeader';
 import StatsContainer from '@sections/Stats/StatsContainer';
 
-const Stats = () => {
-  return (
-    <>
-      <Container className="section-container bg-primary">
-        <SectionHeader title="My activity stats" />
-        <StatsContainer />
-      </Container>
-    </>
-  );
-};
+const Stats = () => (
+  <>
+    <Container className="section-container bg-primary">
+      <SectionHeader title="My activity stats" />
+      <StatsContainer />
+    </Container>
+  </>
+);
 
 export default Stats;
