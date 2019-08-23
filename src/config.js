418cde9b51c19d099f1389c94d01d349507c1c3e
Vitaliy Tkachuk <vitfl@protonmail.com>
2019-08-23 17:34:05 +0300

    :wrench: disabling underConstruction alert on master branch

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   underConstruction: {
-    enable: true,
+    enable: false,
     title: "Website Is Under Development",
     text: `I'm still working on this website, so please understand if you come
     across anything that's broken or incomplete.`,
