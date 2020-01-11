45167750d0fd41cedf60d7a13cdf3fa5635da50b
Vitaliy Tkachuk <vitfl@protonmail.com>
2020-01-12 01:25:59 +0200

    Revert "temp commit"
    This reverts commit bd2239df5ddf69258ed4654a702e31e726f988ad.

--- client/src/hooks/github/useGithubRepos.js
+++ client/src/hooks/github/useGithubRepos.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import * as GITHUB from "@constants/github";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import * as GITHUB from '@constants/github';
 
-const reposInfo = responseJSON => {
+const reposInfo = (responseJSON) => {
   const reposInfoArray = responseJSON.data.items.map(item => ({
     full_name: item.full_name,
     name: item.name,
@@ -10,28 +10,26 @@ const reposInfo = responseJSON => {
     html_url: item.html_url,
     contents_url: item.contents_url,
     created_at: item.created_at,
-    updated_at: item.updated_at
+    updated_at: item.updated_at,
   }));
   return reposInfoArray;
 };
 
 const useGithubRepos = (user, topic) => {
   const [reposList, setreposList] = useState(false);
-  console.log(process.env.REACT_APP_GITHUB_TOKEN);
+
   const instance = axios.create({
     baseURL: GITHUB.restEndpoint,
     timeout: 4000,
     headers: {
-      Accept: "application/vnd.github.v3+json",
-      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-    }
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
   });
   useEffect(() => {
-    instance
-      .get(`/search/repositories?q=user:${user}+topic:${topic}`)
-      .then(response => {
-        setreposList(reposInfo(response));
-      });
+    instance.get(`/search/repositories?q=user:${user}+topic:${topic}`).then((response) => {
+      setreposList(reposInfo(response));
+    });
   }, [user, topic]);
 
   return reposList;
