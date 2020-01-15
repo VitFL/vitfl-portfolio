0292d097017bb47d8538cdee46cffb1a24cdf0ba
Vitaliy Tkachuk <vitfl@protonmail.com>
2020-01-15 22:57:05 +0200

    :recycle: [Front-end] refactoring ScrollToTop component

--- client/src/components/ScrollToTop/ScrollToTop.jsx
+++ client/src/components/ScrollToTop/ScrollToTop.jsx
@@ -3,33 +3,34 @@ import styled from "styled-components";
 import { Icon } from "react-icons-kit";
 import { arrowUp2 } from "react-icons-kit/icomoon/arrowUp2";
 
+const Button = styled.div`
+  position: fixed;
+  bottom: 25px;
+  right: -55px;
+  background-color: var(--light);
+  -webkit-border-radius: 0.2em;
+  border-radius: 0.2em;
+  color: #fff;
+  font-size: 26px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  -webkit-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+  opacity: 0.2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    background-color: var(--accent);
+    opacity: 0.5;
+  }
+  &.active {
+    right: 25px;
+  }
+`;
+
 const ScrollToTop = () => {
-  const Button = styled.div`
-    position: fixed;
-    bottom: 25px;
-    right: -55px;
-    background-color: var(--light);
-    -webkit-border-radius: 0.2em;
-    border-radius: 0.2em;
-    color: #fff;
-    font-size: 26px;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    -webkit-transition: all 0.3s ease;
-    transition: all 0.3s ease;
-    opacity: 0.2;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    &:hover {
-      background-color: var(--accent);
-      opacity: 0.5;
-    }
-    &.active {
-      right: 25px;
-    }
-  `;
 
   const buttonRef = React.createRef();
 
