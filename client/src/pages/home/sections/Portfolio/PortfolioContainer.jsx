1db66c38f17583aa5c9d6b485ea12babde67cef6
Vitaliy Tkachuk <VitFL@protonmail.com>
2020-02-09 22:30:33 +0200

    :iphone: [Front-end] Improve responsiveness for 'Portfolio' section

--- client/src/pages/home/sections/Portfolio/PortfolioContainer.jsx
+++ client/src/pages/home/sections/Portfolio/PortfolioContainer.jsx
@@ -8,7 +8,7 @@ const PortfolioContainer = ({ reposList }) => {
     ? reposList.map((repo, index) => {
       const repoContents = useGithubRepoContents(repo, 'README.md');
       return (
-        <Col key={index} className="col-4 mb-4 d-flex align-items-stretch">
+        <Col key={index} className="col-xl-4 col-md-6 col-10 mb-4 d-flex align-items-stretch">
           <PortfolioItem repo={repo} repoContents={repoContents} />
         </Col>
       );
